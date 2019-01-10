2543435e7ebdf03cc6142fa938fd8885f11fbc58
Nemanja Alavanja <mojomanyana@users.noreply.github.com>
2019-01-10 13:57:13 +0100

    returning data from openweathermap

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
-"use strict"
+import fetch from 'node-fetch';
 
 module.exports = (context, callback) => {
-    callback(undefined, {status: "done"});
+  fetch('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
+    .then(res => res.json())
+    .then(json => callback(undefined, json));
 }
